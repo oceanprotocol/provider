da86997e7a279e8890b158180b174b1100600c7f
ssallam <travis@travis-ci.org>
2020-09-16 14:56:28 +0200

    update ocean-lib version.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ install_requirements = [
     'osmosis-on-premise-driver==0.0.6',
     'osmosis-ipfs-driver==0.0.1',
     'Werkzeug>=0.15.3',
-    'ocean-lib',
+    'ocean-lib>=0.3.4',
     'requests_testadapter',
     'eciespy'
 ]
