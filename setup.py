d200dcad8f0af3ee178241ca6c760874b97840aa
ssallam <travis@travis-ci.org>
2020-09-22 15:02:21 +0200

    update lib-py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ install_requirements = [
     'osmosis-on-premise-driver==0.1.0',
     'osmosis-ipfs-driver==0.1.0',
     'Werkzeug>=0.15.3',
-    'ocean-lib>=0.3.6',
+    'ocean-lib>=0.4.0',
     'requests_testadapter',
     'eciespy'
 ]
