2a75c285f59a633b4eeac1f2538e70419c7049b7
ssallam <travis@travis-ci.org>
2020-10-27 09:02:38 +0100

    update ocean.py version to 0.5.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ install_requirements = [
     'osmosis-on-premise-driver==0.1.0',
     'osmosis-ipfs-driver==0.1.0',
     'Werkzeug>=0.15.3',
-    'ocean-lib>=0.4.8',
+    'ocean-lib>=0.5.0',
     'requests_testadapter',
     'eciespy'
 ]
