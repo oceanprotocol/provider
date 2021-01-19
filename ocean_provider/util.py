001b765f4fc03c11d46525c3f1607aba224bbeef
Calina Cenan <calina.tutunaru@algotech.solutions>
2021-01-20 00:56:15 +0200

    Removes some comments and improve code formatting on util.py.

--- ocean_provider/util.py
+++ ocean_provider/util.py
@@ -39,7 +39,9 @@ def get_request_data(request, url_params_only=False):
     return request.args if request.args else request.json
 
 
-def build_download_response(request, requests_session, url, download_url, content_type=None):
+def build_download_response(
+    request, requests_session, url, download_url, content_type=None
+):
     try:
         download_request_headers = {}
         download_response_headers = {}
@@ -49,13 +51,19 @@ def build_download_response(request, requests_session, url, download_url, conten
             download_request_headers = {"Range": request.headers.get('range')}
             download_response_headers = download_request_headers
 
-        response = requests_session.get(download_url, headers=download_request_headers, stream=True)
+        response = requests_session.get(
+            download_url, headers=download_request_headers, stream=True
+        )
         if not is_range_request:
             filename = url.split("/")[-1]
 
-            content_disposition_header = response.headers.get('content-disposition')
+            content_disposition_header = response.headers.get(
+                'content-disposition'
+            )
             if content_disposition_header:
-                _, content_disposition_params = parse_header(content_disposition_header)
+                _, content_disposition_params = parse_header(
+                    content_disposition_header
+                )
                 content_filename = content_disposition_params.get('filename')
                 if content_filename:
                     filename = content_filename
@@ -110,12 +118,17 @@ def get_asset_files_list(asset, wallet):
 
         return files_list
     except Exception as e:
-        logger.error(f'Error decrypting asset files for asset {asset.did}: {str(e)}')
+        logger.error(
+            f'Error decrypting asset files for asset {asset.did}: {str(e)}'
+        )
         raise
 
 
 def get_asset_url_at_index(url_index, asset, wallet):
-    logger.debug(f'get_asset_url_at_index(): url_index={url_index}, did={asset.did}, provider={wallet.address}')
+    logger.debug(
+        f'get_asset_url_at_index(): url_index={url_index}, '
+        f'did={asset.did}, provider={wallet.address}'
+    )
     try:
         files_list = get_asset_urls(asset, wallet)
         if url_index >= len(files_list):
@@ -123,24 +136,33 @@ def get_asset_url_at_index(url_index, asset, wallet):
         return files_list[url_index]
 
     except Exception as e:
-        logger.error(f'Error decrypting url at index {url_index} for asset {asset.did}: {str(e)}')
+        logger.error(
+            f'Error decrypting url at index {url_index} for '
+            f'asset {asset.did}: {str(e)}'
+        )
         raise
 
 
 def get_asset_urls(asset, wallet):
     """return list of urls of the files included in this `asset` in order
     """
-    logger.debug(f'get_asset_urls(): did={asset.did}, provider={wallet.address}')
+    logger.debug(
+        f'get_asset_urls(): did={asset.did}, provider={wallet.address}'
+    )
     try:
         files_list = get_asset_files_list(asset, wallet)
         input_urls = []
         for i, file_meta_dict in enumerate(files_list):
             if not file_meta_dict or not isinstance(file_meta_dict, dict):
-                raise TypeError(f'Invalid file meta at index {i}, expected a dict, got a '
-                                f'{type(file_meta_dict)}.')
+                raise TypeError(
+                    f'Invalid file meta at index {i}, expected a dict, got a '
+                    f'{type(file_meta_dict)}.'
+                )
             if 'url' not in file_meta_dict:
-                raise ValueError(f'The "url" key is not found in the '
-                                 f'file dict {file_meta_dict} at index {i}.')
+                raise ValueError(
+                    f'The "url" key is not found in the '
+                    f'file dict {file_meta_dict} at index {i}.'
+                )
 
             input_urls.append(file_meta_dict['url'])
 
@@ -179,7 +201,9 @@ def check_required_attributes(required_attributes, data, method):
         return 'payload seems empty.', 400
     for attr in required_attributes:
         if attr not in data:
-            logger.error('%s request failed: required attr %s missing.' % (method, attr))
+            logger.error(
+                '%s request failed: required attr %s missing.' % (method, attr)
+            )
             return '"%s" is required in the call to %s' % (attr, method), 400
     return None, None
 
@@ -202,7 +226,9 @@ def validate_order(sender, token_address, num_tokens, tx_id, did, service_id):
                 raise
 
 
-def validate_transfer_not_used_for_other_service(did, service_id, transfer_tx_id, consumer_address, token_address):
+def validate_transfer_not_used_for_other_service(
+    did, service_id, transfer_tx_id, consumer_address, token_address
+):
     logger.debug(
         f'validate_transfer_not_used_for_other_service: '
         f'did={did}, service_id={service_id}, transfer_tx_id={transfer_tx_id}, '
@@ -211,7 +237,9 @@ def validate_transfer_not_used_for_other_service(did, service_id, transfer_tx_id
     return
 
 
-def record_consume_request(did, service_id, order_tx_id, consumer_address, token_address, amount):
+def record_consume_request(
+    did, service_id, order_tx_id, consumer_address, token_address, amount
+):
     logger.debug(
         f'record_consume_request: '
         f'did={did}, service_id={service_id}, transfer_tx_id={order_tx_id}, '
@@ -228,32 +256,30 @@ def process_consume_request(data: dict, method: str):
     service_id = data.get('serviceId')
     service_type = data.get('serviceType')
 
-    # grab asset for did from the metadatastore associated with the Data Token address
+    # grab asset for did from the metadatastore associated with
+    # the Data Token address
     asset = get_asset_from_metadatastore(get_metadata_url(), did)
     service = ServiceAgreement.from_ddo(service_type, asset)
     if service.type != service_type:
         raise AssertionError(
-            f'Requested service with id {service_id} has type {service.type} which '
-            f'does not match the requested service type {service_type}.'
+            f'Requested service with id {service_id} has type {service.type} '
+            f'which does not match the requested service type {service_type}.'
         )
-
-    # if require_signature:
-    #     assert user_nonce, '`user_nonce` is required when signature is required.'
-    #     # Raises ValueError when signature is invalid
-    #     signature = data.get('signature')
-    #     verify_signature(consumer_address, signature, did, user_nonce.get_nonce(consumer_address))
 
     return asset, service, did, consumer_address, token_address
 
 
 def process_compute_request(
-    data, user_nonce: UserNonce, require_signature: bool=True, with_validation=False
+    data, user_nonce: UserNonce, require_signature: bool=True,
+    with_validation=False
 ):
     if with_validation:
         required_attributes = ['consumerAddress']
         if require_signature:
             required_attributes.append('signature')
-        msg, status = check_required_attributes(required_attributes, data, 'compute')
+        msg, status = check_required_attributes(
+            required_attributes, data, 'compute'
+        )
         if msg:
             raise BadRequestError(msg)
 
@@ -276,34 +302,50 @@ def process_compute_request(
     # Consumer signature
     if require_signature:
         signature = data.get('signature')
-        original_msg = f'{body.get("owner", "")}{body.get("jobId", "")}{body.get("documentId", "")}'
-        verify_signature(owner, signature, original_msg, user_nonce.get_nonce(owner))
+        original_msg = f'{body.get("owner", "")}{body.get("jobId", "")}{body.get("documentId", "")}'  # noqa
+        verify_signature(
+            owner, signature, original_msg, user_nonce.get_nonce(owner)
+        )
 
-    msg_to_sign = f'{provider_wallet.address}{body.get("jobId", "")}{body.get("documentId", "")}'
+    msg_to_sign = f'{provider_wallet.address}{body.get("jobId", "")}{body.get("documentId", "")}'  # noqa
     msg_hash = add_ethereum_prefix_and_hash_msg(msg_to_sign)
     body['providerSignature'] = Web3Helper.sign_hash(msg_hash, provider_wallet)
     return body
 
 
-def build_stage_algorithm_dict(consumer_address, algorithm_did, algorithm_token_address, algorithm_tx_id,
-                               algorithm_meta, provider_wallet, receiver_address=None):
+def build_stage_algorithm_dict(
+    consumer_address, algorithm_did, algorithm_token_address, algorithm_tx_id,
+    algorithm_meta, provider_wallet, receiver_address=None
+):
     if algorithm_did is not None:
         assert algorithm_token_address and algorithm_tx_id, \
-            'algorithm_did requires both algorithm_token_address and algorithm_tx_id.'
+            'algorithm_did requires both algorithm_token_address and algorithm_tx_id.'  # noqa
 
-        algo_asset = get_asset_from_metadatastore(get_metadata_url(), algorithm_did)
+        algo_asset = get_asset_from_metadatastore(
+            get_metadata_url(), algorithm_did
+        )
 
-        service = ServiceAgreement.from_ddo(ServiceTypes.ASSET_ACCESS, algo_asset)
+        service = ServiceAgreement.from_ddo(
+            ServiceTypes.ASSET_ACCESS, algo_asset
+        )
         _tx, _order_log, _transfer_log = validate_order(
             consumer_address,
             algorithm_token_address,
             float(service.get_cost()),
             algorithm_tx_id,
-            add_0x_prefix(did_to_id(algorithm_did)) if algorithm_did.startswith('did:') else algorithm_did,
+            add_0x_prefix(
+                did_to_id(algorithm_did)
+            ) if algorithm_did.startswith('did:') else algorithm_did,
             service.index
         )
-        validate_transfer_not_used_for_other_service(algorithm_did, service.index, algorithm_tx_id, consumer_address, algorithm_token_address)
-        record_consume_request(algorithm_did, service.index, algorithm_tx_id, consumer_address, algorithm_token_address, service.get_cost())
+        validate_transfer_not_used_for_other_service(
+            algorithm_did, service.index, algorithm_tx_id,
+            consumer_address, algorithm_token_address
+        )
+        record_consume_request(
+            algorithm_did, service.index, algorithm_tx_id, consumer_address,
+            algorithm_token_address, service.get_cost()
+        )
 
         algo_id = algorithm_did
         raw_code = ''
@@ -325,14 +367,18 @@ def build_stage_algorithm_dict(consumer_address, algorithm_did, algorithm_token_
 
 def build_stage_output_dict(output_def, asset, owner, provider_wallet):
     config = get_config()
-    service_endpoint = asset.get_service(ServiceTypes.CLOUD_COMPUTE).service_endpoint
+    service_endpoint = asset.get_service(
+        ServiceTypes.CLOUD_COMPUTE
+    ).service_endpoint
     if BaseURLs.ASSETS_URL in service_endpoint:
         service_endpoint = service_endpoint.split(BaseURLs.ASSETS_URL)[0]
 
     return dict({
         'nodeUri': output_def.get('nodeUri', config.network_url),
         'brizoUri': output_def.get('brizoUri', service_endpoint),
-        'brizoAddress': output_def.get('brizoAddress', provider_wallet.address),
+        'brizoAddress': output_def.get(
+            'brizoAddress', provider_wallet.address
+        ),
         'metadata': output_def.get('metadata', dict({
             'main': {
                 'name': 'Compute job output'
@@ -368,12 +414,16 @@ def validate_algorithm_dict(algorithm_dict, algorithm_did):
         return f'cannot get url for the algorithmDid {algorithm_did}', 400
 
     if not algorithm_dict['url'] and not algorithm_dict['rawcode']:
-        return f'`algorithmMeta` must define one of `url` or `rawcode`, but both seem missing.', 400
+        return f'`algorithmMeta` must define one of `url` or `rawcode`, but both seem missing.', 400  # noqa
 
     container = algorithm_dict['container']
     # Validate `container` data
-    if not (container.get('entrypoint') and container.get('image') and container.get('tag')):
-        return f'algorithm `container` must specify values for all of entrypoint, image and tag.', 400
+    if not (
+        container.get('entrypoint') and
+        container.get('image') and
+        container.get('tag')
+    ):
+        return f'algorithm `container` must specify values for all of entrypoint, image and tag.', 400  # noqa
 
     return None, None
 
@@ -381,7 +431,8 @@ def validate_algorithm_dict(algorithm_dict, algorithm_did):
 def check_url_details(url):
     """
     If the url argument is invalid, returns False and empty dictionary.
-    Otherwise it returns True and a dictionary containing contentType and contentLength.
+    Otherwise it returns True and a dictionary containing contentType and
+    contentLength.
     """
     try:
         result = requests.options(url)
@@ -398,7 +449,10 @@ def check_url_details(url):
             content_length = result.headers.get('Content-Length')
 
             if content_type and content_length:
-                return True, {"contentLength": content_length, "contentType": content_type}
+                return True, {
+                    "contentLength": content_length,
+                    "contentType": content_type
+                }
 
     except requests.exceptions.InvalidSchema:
         pass
