3585edb24714683e450516c7e264df4ddaa57f75
Calina Cenan <calina.tutunaru@algotech.solutions>
2021-01-21 21:07:45 +0200

    Update to Python 3.8

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
     ],
     description="🐳 Ocean Provider.",
     extras_require={
