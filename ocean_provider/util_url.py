094830924ef4a91a81e711ac290c0a4185755c82
Calina Cenan <calina.tutunaru@algotech.solutions>
2021-01-28 11:21:40 +0200

    Add tests and positive validation for DNS.

--- ocean_provider/util_url.py
+++ ocean_provider/util_url.py
@@ -11,7 +11,9 @@ logger = logging.getLogger(__name__)
 def is_safe_url(url):
     if not is_safe_schema(url):
         return False
+
     result = urlparse(url)
+
     return is_safe_domain(result.netloc)
 
 
@@ -23,51 +25,67 @@ def is_safe_schema(url):
         return False
 
 
-def is_safe_domain(domain):
+def _get_records(domain, record_type):
     DNS_RESOLVER = dns.resolver.Resolver()
     try:
-        ip_v4_records = DNS_RESOLVER.resolve(domain, "A", search=True)
+        return DNS_RESOLVER.resolve(domain, record_type, search=True)
     except Exception as e:
-        ip_v4_records = None
         logger.info(
-            "[i] Cannot get A record for domain '%s': %s\n" % (domain, e))
-    try:
-        ip_v6_records = DNS_RESOLVER.resolve(domain, "AAAA", search=True)
-    except Exception as e:
-        ip_v6_records = None
-        logger.info(
-            "[i] Cannot get AAAA record for domain '%s': %s\n" % (domain, e))
+            f"[i] Cannot get {record_type} record for domain {domain}: {e}\n"
+        )
 
-    if verify_dns_records(domain, ip_v4_records, "A") or verify_dns_records(domain, ip_v6_records, "AAAA"):
-        return False
+        return None
+
+
+def is_safe_domain(domain):
+    ip_v4_records = _get_records(domain, "A")
+    ip_v6_records = _get_records(domain, "AAAA")
+
+    return (
+        validate_dns_records(domain, ip_v4_records, "A") and
+        validate_dns_records(domain, ip_v6_records, "AAAA")
+    )
+
+
+def validate_dns_records(domain, records, record_type):
+    """
+    Verify if all DNS records resolve to public IP addresses.
+    Return True if they do, False if any error has been detected.
+    """
+    if records is None:
+        return True
+
+    for record in records:
+        if not validate_dns_record(record, domain, record_type):
+            return False
+
     return True
 
 
-def verify_dns_records(domain, records, type):
-    """
-    Verify if one of the DNS records resolve to a non public IP address.
-    Return a boolean indicating if any error has been detected.
-    """
-    if records is None:
-        return False
+def validate_dns_record(record, domain, record_type):
+    value = record.to_text().strip()
     allow_non_public_ip = get_config().allow_non_public_ip
-    error_detected = True
-    for record in records:
-        value = record.to_text().strip()
-        try:
-            ip = ipaddress.ip_address(value)
-            # See https://docs.python.org/3/library/ipaddress.html#ipaddress.IPv4Address.is_global
-            if allow_non_public_ip and (ip.is_private or ip.is_reserved or ip.is_loopback):
-                logger.warning("[!] DNS record type '%s' for domain name '%s' resolve to a non public IP address '%s', but allowed by config!" % (
-                    type, domain, value))
-                error_detected = False
-            if ip.is_global:
-                error_detected = False
-            if error_detected == True:
-                logger.error("[!] DNS record type '%s' for domain name '%s' resolve to a non public IP address '%s'!" % (
-                    type, domain, value))
 
-        except ValueError:
-            error_detected = True
-            logger.info("[!] '%s' is not valid IP address!" % value)
-    return error_detected
+    try:
+        ip = ipaddress.ip_address(value)
+        # noqa See https://docs.python.org/3/library/ipaddress.html#ipaddress.IPv4Address.is_global
+        if ip.is_private or ip.is_reserved or ip.is_loopback:
+            if allow_non_public_ip:
+                logger.warning(
+                    f"[!] DNS record type {record_type} for domain name "
+                    f"{domain} resolves to a non public IP address {value}, "
+                    "but allowed by config!"
+                )
+            else:
+                logger.error(
+                    f"[!] DNS record type {record_type} for domain name "
+                    f"{domain} resolves to a non public IP address {value}, "
+                    "but allowed by config!"
+                )
+
+                return False
+    except ValueError:
+        logger.info("[!] '%s' is not valid IP address!" % value)
+        return False
+
+    return True
