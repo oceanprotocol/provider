395ea3190cee44c1c1177c5f2bd702006075ded3
Călina Cenan <calina@kaolin.tech>
2021-02-24 10:21:58 +0200

    Update myapp.py

--- ocean_provider/myapp.py
+++ ocean_provider/myapp.py
@@ -8,7 +8,6 @@ from os.path import abspath, dirname
 from flask import Flask
 from flask_cors import CORS
 from flask_sieve import Sieve
-from ocean_provider.models import db as db_models  # noqa isort: skip
 from ocean_provider.utils.basics import get_config
 
 app = Flask(__name__)
@@ -27,7 +26,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////" + os.path.join(
     PROJECT_ROOT, "db", get_config().storage_path
 )
 
-
+from ocean_provider.models import db as db_models  # noqa isort: skip
 db = db_models
 
 try:
