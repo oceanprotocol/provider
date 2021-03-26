942f9e378e421905f5308c8ee6aa473906977a47
Akshay <akshay.ap95@gmail.com>
2021-03-26 22:27:45 +0100

    Docs: Fix build issue

--- ocean_provider/routes/consume.py
+++ ocean_provider/routes/consume.py
@@ -255,13 +255,13 @@ def initialize():
 
     :return:
         json object as follows:
-        {
+        ```{
             "from": <consumer-address>,
             "to": <receiver-address>,
             "numTokens": <tokens-amount-in-base>
             "dataToken": <data-token-contract-address>,
             "nonce": <nonce-used-in-consumer-signature>
-        }
+        }```
     """
     data = get_request_data(request)
 
