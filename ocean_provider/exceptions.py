4808a6319091831962a669114ada86cd86d8191c
Calina Cenan <calina@oceanprotocol.com>
2021-05-13 09:39:21 +0300

    Fix black.

--- ocean_provider/exceptions.py
+++ ocean_provider/exceptions.py
@@ -2,5 +2,7 @@
 # Copyright 2021 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
+
+
 class InvalidSignatureError(Exception):
-    """ User signature is not valid."""
+    """User signature is not valid."""
