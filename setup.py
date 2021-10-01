1cce0a2c29877f26b3f5fd86da385bf6dcf49750
Călina Cenan <calina@kaolin.tech>
2021-10-01 12:51:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ with open("README.md") as readme_file:
 install_requirements = [
     # Install ocean-utils first
     "ocean-contracts==0.6.7",
+    "web3==5.19.0",
     "Flask==1.1.2",
     "Flask-Cors==3.0.9",
     "Flask-RESTful==0.3.8",
