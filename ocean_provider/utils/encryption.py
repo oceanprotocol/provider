172faeb22ade8e8574fd7bd8d4d7bc1aa3b0064e
David Hunt-Mateo <david@oceanprotocol.com>
2021-11-02 10:35:49 -0400

    Add support for text input to do_encrypt

--- ocean_provider/utils/encryption.py
+++ ocean_provider/utils/encryption.py
@@ -7,12 +7,15 @@ from typing import Union
 import ecies
 from eth_account.account import LocalAccount
 from eth_typing.encoding import HexStr
+from eth_utils.hexadecimal import is_0x_prefixed
 from ocean_provider.utils.accounts import get_private_key
 from web3 import Web3
 
 
 def do_encrypt(
-    document: Union[HexStr, bytes], wallet: LocalAccount = None, public_key: str = None
+    document: Union[HexStr, str, bytes],
+    wallet: LocalAccount = None,
+    public_key: str = None,
 ) -> HexStr:
     """
     :param document: document to be encrypted as HexStr or bytes
@@ -22,7 +25,10 @@ def do_encrypt(
     """
     key = get_private_key(wallet).public_key.to_hex() if wallet else public_key
     if isinstance(document, str):
-        document = Web3.toBytes(hexstr=document)
+        if is_0x_prefixed(document):
+            document = Web3.toBytes(hexstr=document)
+        else:
+            document = Web3.toBytes(text=document)
     encrypted_document = ecies.encrypt(key, document)
     return Web3.toHex(encrypted_document)
 
