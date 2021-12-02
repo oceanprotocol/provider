be8fb2a689ba6ca4b11c03ac600b033ba6a55b92
Calina Cenan <calina@cenan.net>
2021-12-02 13:14:28 +0000

    Revert an accidental change.

--- tests/test_fileinfo.py
+++ tests/test_fileinfo.py
@@ -36,7 +36,7 @@ def test_asset_info(client, publisher_wallet):
         )
         assert file_info["checksumType"] == "sha256"
 
-    asset = get_registered_asset(publisher_wallet)
+    asset = get_dataset_with_invalid_url_ddo(client, publisher_wallet)
     service = asset.get_service_by_type(ServiceType.ACCESS)
     response = client.post(
         fileinfo_url, json={"did": asset.did, "serviceId": service.id}
