7d3f5c3861fcce291ce175ebfb20c6408323ffeb
Calina Cenan <calina@cenan.net>
2021-12-06 11:41:42 +0000

    Intermediary commit.

--- ocean_provider/models.py
+++ ocean_provider/models.py
@@ -10,11 +10,9 @@ from .database import Base
 class UserNonce(Base):
     """
     Table for storing the nonce values for the Eth account addresses.
-    Also, defines `FIRST_NONCE = 0` as default value.
     """
 
     __tablename__ = "user_nonce"
-    FIRST_NONCE = "0"
 
     address = Column(String(255), nullable=False, primary_key=True, autoincrement=False)
     nonce = Column(String(255), nullable=False)
