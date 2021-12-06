19398910eb6b755559f7ad009ce2d9b7bd856036
Calina Cenan <calina@cenan.net>
2021-12-06 09:48:37 +0000

    Adds Redis option for nonce.

--- ocean_provider/test/test_user_nonce.py
+++ ocean_provider/test/test_user_nonce.py
@@ -10,7 +10,9 @@ from ocean_provider import models, user_nonce
 from ocean_provider.user_nonce import get_nonce, increment_nonce
 
 
-def test_get_and_increment_nonce(publisher_address, consumer_address):
+def test_get_and_increment_nonce(monkeypatch, publisher_address, consumer_address):
+    # pass through sqlite
+    monkeypatch.delenv("REDIS_CONNECTION")
 
     # get_nonce can be used on addresses that are not in the user_nonce table
     assert get_nonce("0x0000000000000000000000000000000000000000") == "0"
@@ -29,7 +31,28 @@ def test_get_and_increment_nonce(publisher_address, consumer_address):
     assert int(get_nonce(publisher_address)) == publisher_nonce_int + 2
 
 
-def test_increment_nonce_exception(publisher_address):
+def test_get_and_increment_nonce_redis(monkeypatch, publisher_address, consumer_address):
+    # get_nonce can be used on addresses that are not in the user_nonce table
+    assert get_nonce("0x0000000000000000000000000000000000000000") == "0"
+
+    # get_nonce doesn't affect the value of nonce
+    publisher_nonce = get_nonce(publisher_address)
+    publisher_nonce_int = int(publisher_nonce)
+    assert get_nonce(publisher_address) == publisher_nonce
+
+    # increment_nonce increases the nonce by 1
+    increment_nonce(publisher_address)
+    assert int(get_nonce(publisher_address)) == publisher_nonce_int + 1
+
+    # increment_nonce can be used twice in a row
+    increment_nonce(publisher_address)
+    assert int(get_nonce(publisher_address)) == publisher_nonce_int + 2
+
+
+def test_increment_nonce_exception(monkeypatch, publisher_address):
+    # pass through sqlite
+    monkeypatch.delenv("REDIS_CONNECTION")
+
     # Ensure address exists in database
     increment_nonce(publisher_address)
 
--- ocean_provider/user_nonce.py
+++ ocean_provider/user_nonce.py
@@ -2,7 +2,9 @@
 # Copyright 2021 Ocean Protocol Foundation
 # SPDX-License-Identifier: Apache-2.0
 #
+from flask_caching import Cache
 import logging
+import os
 
 from ocean_provider import models
 from ocean_provider.myapp import app
@@ -10,11 +12,21 @@ from ocean_provider.myapp import app
 logger = logging.getLogger(__name__)
 db = app.session
 
+cache = Cache(app, config={
+    "CACHE_TYPE": "redis",
+    "CACHE_KEY_PREFIX": "ocean_provider",
+    "CACHE_REDIS_URL": os.getenv("REDIS_CONNECTION")
+})
+
 
 def get_nonce(address):
     """
     :return: `nonce` for the given address stored in the database
     """
+    if os.getenv("REDIS_CONNECTION"):
+        result = cache.get(address)
+        return result if result else models.UserNonce.FIRST_NONCE
+
     result = models.UserNonce.query.filter_by(address=address).first()
 
     return result.nonce if result else models.UserNonce.FIRST_NONCE
@@ -25,6 +37,13 @@ def increment_nonce(address):
     Increments the value of `nonce` in the database
     :param: address
     """
+    if os.getenv("REDIS_CONNECTION"):
+        nonce = get_or_create_user_nonce_object(address)
+        nonce = int(nonce) + 1
+        cache.set(address, nonce)
+
+        return
+
     nonce_object = get_or_create_user_nonce_object(address)
     nonce_value = nonce_object.nonce
     incremented_nonce = int(nonce_value) + 1
@@ -44,6 +63,12 @@ def increment_nonce(address):
 
 
 def get_or_create_user_nonce_object(address):
+    if os.getenv("REDIS_CONNECTION"):
+        nonce = cache.get(address)
+        nonce = nonce if nonce else models.UserNonce.FIRST_NONCE
+        cache.set(address, nonce)
+        return nonce
+
     nonce_object = models.UserNonce.query.filter_by(address=address).first()
     if nonce_object is None:
         nonce_object = models.UserNonce(
