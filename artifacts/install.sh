f8a4aad597de4d433b580885ae9e96c18e2ad2f3
David Hunt-Mateo <david@oceanprotocol.com>
2021-12-07 09:09:06 -0500

    Remove line that copies `artifacts/addresses` which no longer exists
      * Also Add license header

--- artifacts/install.sh
+++ artifacts/install.sh
@@ -1,5 +1,8 @@
+##
+## Copyright 2021 Ocean Protocol Foundation
+## SPDX-License-Identifier: Apache-2.0
+##
 rm -rf $VIRTUAL_ENV/lib/python3.9/site-packages/artifacts/
 mkdir ${VIRTUAL_ENV}/lib/python3.9/site-packages/artifacts/
-cp -r ./addresses $VIRTUAL_ENV/lib/python3.9/site-packages/artifacts/addresses
 
 find ./artifacts -name '*.json' -exec cp -prv '{}' ${VIRTUAL_ENV}'/lib/python3.9/site-packages/artifacts/' ';'
