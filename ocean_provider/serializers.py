f7195e691045451485e34cc1b16fd9d261de7f4f
alexcos20 <alex.coseru@gmail.com>
2021-12-30 05:33:07 -0800

    move algouserdata to remote

--- ocean_provider/serializers.py
+++ ocean_provider/serializers.py
@@ -26,7 +26,7 @@ class StageAlgoSerializer:
             "id": None,
             "rawcode": None,
             "container": None,
-            "algocustomdata": None,
+            "algocustomdata": self.algo_data.get("algocustomdata", None),
         }
 
         if algorithm_meta and isinstance(algorithm_meta, str):
@@ -56,9 +56,8 @@ class StageAlgoSerializer:
                 "serviceEndpoint": self.algo_service.service_endpoint,
                 "txId": algorithm_tx_id,
                 "serviceIndex": self.algo_service.index,
+                "algouserdata": self.algo_data.get("algouserdata", None),
             }
-        dict_template["algouserdata"] = self.algo_data.get("algouserdata", None)
-        dict_template["algocustomdata"] = self.algo_data.get("algocustomdata", None)
         dict_template["container"] = algo_asset.metadata["main"]["algorithm"][
             "container"
         ]
