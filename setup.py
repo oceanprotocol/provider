565788c8ee971e0da5ae4ab0b2d042460e044ad7
Calina Cenan <calina@cenan.net>
2022-03-11 10:16:53 +0000

    Bump version: 0.4.22 → 0.4.23

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
     url="https://github.com/oceanprotocol/provider-py",
     # fmt: off
     # bumpversion needs single quotes
-    version='0.4.22',
+    version='0.4.23',
     # fmt: on
     zip_safe=False,
 )
