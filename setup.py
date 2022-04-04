49c9a5677a696e973f3ceac967489aadbaf23bea
alexcos20 <alex.coseru@gmail.com>
2022-04-04 05:32:15 -0700

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ setup(
     url="https://github.com/oceanprotocol/provider-py",
     # fmt: off
     # bumpversion needs single quotes
-    version='1.0.2',
+    version='1.0.3',
     # fmt: on
     zip_safe=False,
 )
