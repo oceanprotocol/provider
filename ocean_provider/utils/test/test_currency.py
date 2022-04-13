0c4225a3cd58f1de789f7b7c560a360b20b6925c
David Hunt-Mateo <david@oceanprotocol.com>
2022-04-13 16:12:59 -0400

    Fix bug where to_wei didn't handle decimals correctly

--- ocean_provider/utils/test/test_currency.py
+++ ocean_provider/utils/test/test_currency.py
@@ -5,15 +5,23 @@
 from decimal import Decimal, localcontext
 
 import pytest
-
 from ocean_provider.utils.currency import (
     ETHEREUM_DECIMAL_CONTEXT,
     MAX_ETHER,
     MAX_WEI,
     MIN_ETHER,
     MIN_WEI,
+    parse_units,
     to_wei,
 )
+
+USDT_DECIMALS = 6
+MIN_USDT = Decimal("0.000001")
+MAX_USDT = Decimal(MAX_WEI).scaleb(-USDT_DECIMALS, context=ETHEREUM_DECIMAL_CONTEXT)
+
+SEVEN_DECIMALS = 7
+MIN_SEVEN = Decimal("0.0000001")
+MAX_SEVEN = Decimal(MAX_WEI).scaleb(-SEVEN_DECIMALS, context=ETHEREUM_DECIMAL_CONTEXT)
 
 
 @pytest.mark.unit
@@ -52,13 +60,37 @@ def test_to_wei():
         with localcontext(ETHEREUM_DECIMAL_CONTEXT):
             to_wei(MAX_ETHER + 1)
 
-    USDT_DECIMALS = 6
-    assert (
-        to_wei("0", USDT_DECIMALS) == 0
-    ), "Zero ether of USDT should equal zero wei of USDT"
-    assert (
-        to_wei("0.123456789123456789", USDT_DECIMALS) == 123456000_000000000
-    ), "Conversion from ether to wei using decimals failed"
-    assert (
-        to_wei("1.123456789123456789", USDT_DECIMALS) == 1_123456000_000000000
-    ), "Conversion from ether to wei using decimals failed"
+
+@pytest.mark.unit
+def test_parse_units():
+    """Test the parse_units function"""
+    assert parse_units("0", USDT_DECIMALS) == 0
+    assert parse_units("0.123456789123456789", USDT_DECIMALS) == 123456
+    assert parse_units("1.123456789123456789", USDT_DECIMALS) == 1_123456
+    assert parse_units("5278.02", USDT_DECIMALS) == 5278_020000
+    assert parse_units(MIN_USDT, USDT_DECIMALS) == MIN_WEI
+    assert parse_units(MAX_USDT, USDT_DECIMALS) == MAX_WEI
+
+    with pytest.raises(ValueError):
+        # Use ETHEREUM_DECIMAL_CONTEXT when performing arithmetic on MAX_USDT
+        with localcontext(ETHEREUM_DECIMAL_CONTEXT):
+            parse_units(MAX_USDT + 1, USDT_DECIMALS)
+
+    assert parse_units("0", "mwei") == 0
+    assert parse_units("0.123456789123456789", "mwei") == 123456
+    assert parse_units("1.123456789123456789", "mwei") == 1_123456
+    assert parse_units("5278.02", "mwei") == 5278_020000
+    assert parse_units(MIN_USDT, "mwei") == MIN_WEI
+    assert parse_units(MAX_USDT, "mwei") == MAX_WEI
+
+    with pytest.raises(ValueError):
+        # Use ETHEREUM_DECIMAL_CONTEXT when performing arithmetic on MAX_USDT
+        with localcontext(ETHEREUM_DECIMAL_CONTEXT):
+            parse_units(MAX_USDT + 1, "mwei")
+
+    assert parse_units("0", SEVEN_DECIMALS) == 0
+    assert parse_units("0.123456789", SEVEN_DECIMALS) == 1234567
+    assert parse_units("1.123456789", SEVEN_DECIMALS) == 1_1234567
+    assert parse_units("5278.02", SEVEN_DECIMALS) == 5278_0200000
+    assert parse_units(MIN_SEVEN, SEVEN_DECIMALS) == MIN_WEI
+    assert parse_units(MAX_SEVEN, SEVEN_DECIMALS) == MAX_WEI
