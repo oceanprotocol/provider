475c92e18389d2eb459e60f3864e511be40e91a3
Alex Coseru <alex.coseru@gmail.com>
2022-05-17 10:53:59 +0300

    bump contracts to alpha 33

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ install_requirements = [
     "flask-swagger-ui==3.25.0",
     "Jinja2>=2.10.1,<3.1",
     "gunicorn==20.0.4",
-    "ocean-contracts==1.0.0a32",
+    "ocean-contracts==1.0.0a33",
     "coloredlogs==15.0.1",
     "Werkzeug==2.0.3",
     "requests_testadapter",
