7607d3ab06030b2b95add30a3d31b79ed014fc3f
Călina Cenan <calina@cenan.net>
2022-05-30 15:02:36 +0300

    Adds initialize access check for reuse. (#462)

--- tests/test_initialize.py
+++ tests/test_initialize.py
@@ -7,7 +7,7 @@ import time
 
 import pytest
 from ocean_provider.constants import BaseURLs
-from ocean_provider.utils.provider_fees import get_c2d_environments
+from ocean_provider.utils.provider_fees import get_c2d_environments, get_provider_fees
 from ocean_provider.utils.services import ServiceType
 from tests.helpers.compute_helpers import (
     build_and_send_ddo_with_compute_service,
@@ -16,13 +16,17 @@ from tests.helpers.compute_helpers import (
 from tests.test_helpers import (
     get_dataset_ddo_disabled,
     get_dataset_ddo_with_denied_consumer,
+    get_dataset_ddo_with_multiple_files,
     get_dataset_with_invalid_url_ddo,
     get_dataset_with_ipfs_url_ddo,
     get_first_service_by_type,
     get_registered_asset,
     initialize_service,
     mint_100_datatokens,
+    start_order,
 )
+
+from unittest.mock import patch
 
 
 @pytest.mark.integration
@@ -96,6 +100,53 @@ def test_initialize_on_asset_with_custom_credentials(
         response.json["error"]
         == f"Error: Access to asset {asset.did} was denied with code: ConsumableCodes.CREDENTIAL_IN_DENY_LIST."
     )
+
+
+@pytest.mark.integration
+def test_initialize_reuse(client, publisher_wallet, consumer_wallet, web3):
+    asset = get_dataset_ddo_with_multiple_files(client, publisher_wallet)
+
+    service = get_first_service_by_type(asset, ServiceType.ACCESS)
+
+    mint_100_datatokens(
+        web3, service.datatoken_address, consumer_wallet.address, publisher_wallet
+    )
+
+    tx_id, _ = start_order(
+        web3,
+        service.datatoken_address,
+        consumer_wallet.address,
+        service.index,
+        get_provider_fees(asset.did, service, consumer_wallet.address, 0),
+        consumer_wallet,
+    )
+
+    response = initialize_service(
+        client,
+        asset.did,
+        service,
+        consumer_wallet,
+        raw_response=True,
+        reuse_order=tx_id,
+    )
+
+    assert "datatoken" not in response.json
+    assert response.json["validOrder"] == tx_id
+
+    with patch("web3.eth.wait_for_transaction_receipt") as mock:
+        # speed up with mocks, otherwise this test waits a lot until reaching the Exception
+        mock.side_effect = Exception("Boom!")
+        response = initialize_service(
+            client,
+            asset.did,
+            service,
+            consumer_wallet,
+            raw_response=True,
+            reuse_order=tx_id,
+        )
+
+    assert response.json["datatoken"] == service.datatoken_address
+    assert "validOrder" not in response.json
 
 
 @pytest.mark.integration
