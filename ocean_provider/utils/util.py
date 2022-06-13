f99a53f88d95684a68748a96bad677968f992482
Calina Cenan <calina@cenan.net>
2022-06-13 06:09:06 +0000

    Return error with unlowered method name, add test.

--- ocean_provider/utils/util.py
+++ ocean_provider/utils/util.py
@@ -58,8 +58,10 @@ def build_download_response(
         if is_range_request:
             download_request_headers = {"Range": request.headers.get("range")}
             download_response_headers = download_request_headers
+
         if method.lower() not in ["get", "post"]:
-            raise ValueError(f"Unsafe method {method.lower()}")
+            raise ValueError(f"Unsafe method {method}")
+
         method = getattr(requests_session, method.lower())
         response = method(
             download_url, headers=download_request_headers, stream=True, timeout=3
