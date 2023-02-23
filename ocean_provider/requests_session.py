183d976333022c2c3c85ea9f60bedebf28336a51
Alex Coseru <alex.coseru@gmail.com>
2023-02-23 19:05:54 +0200

    Increase no of connections

--- ocean_provider/requests_session.py
+++ ocean_provider/requests_session.py
@@ -16,13 +16,13 @@ def get_requests_session() -> Session:
     session.mount(
         "http://",
         HTTPAdapter(
-            pool_connections=25, pool_maxsize=25, pool_block=True, max_retries=1
+            pool_connections=100, pool_maxsize=25, pool_block=True, max_retries=5
         ),
     )
     session.mount(
         "https://",
         HTTPAdapter(
-            pool_connections=25, pool_maxsize=25, pool_block=True, max_retries=1
+            pool_connections=100, pool_maxsize=25, pool_block=True, max_retries=5
         ),
     )
     return session
