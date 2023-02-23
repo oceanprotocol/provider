3f98368f5ac8310979bae7ac48aa7efaf043094b
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-02-23 18:56:20 +0200

    retry mechanism for getting c2d envs from op serv.

--- ocean_provider/utils/compute_environments.py
+++ ocean_provider/utils/compute_environments.py
@@ -2,6 +2,8 @@ import os
 from typing import List
 from urllib.parse import urljoin
 
+import requests
+
 from ocean_provider.requests_session import get_requests_session
 from ocean_provider.utils.address import get_provider_fee_token
 from ocean_provider.utils.basics import get_config, get_web3
@@ -19,14 +21,24 @@ def get_c2d_environments() -> List:
         return []
     print(f"os env: {os.getenv('OPERATOR_SERVICE_URL')}")
 
-    standard_headers = {"Content-type": "application/json"}
+    standard_headers = {"Content-type": "application/json", "Connection": "close"}
     web3 = get_web3()
     params = {"chainId": web3.eth.chain_id}
     print(f"c2d env endpoint: {get_compute_environments_endpoint()}")
-    response = requests_session.get(
-        get_compute_environments_endpoint(), headers=standard_headers, params=params
-    )
+    retries = 2
+    while retries != 0:
+        try:
+            response = requests_session.get(
+                get_compute_environments_endpoint(),
+                headers=standard_headers,
+                params=params,
+            )
+            break
+        except requests.exceptions.ConnectionError:
+            retries -= 1
+            continue
 
+    assert response, "Response not found after retrying"
     # loop envs and add provider token from config
     envs = response.json()
     for env in envs:
