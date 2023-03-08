cd442c7f6174806972532526574395c942f95249
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-03-08 02:30:18 +0200

    removed string escape.

--- ocean_provider/utils/compute_environments.py
+++ ocean_provider/utils/compute_environments.py
@@ -14,7 +14,7 @@ requests_session = get_requests_session()
 def get_compute_environments_endpoint():
     url = get_config().operator_service_url + "api/v1/operator/environments"
     print(f"url: {url}\n")
-    return url
+    return url.decode("string_escape")
 
 
 def get_c2d_environments() -> List:
