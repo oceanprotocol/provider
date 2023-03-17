93333fac0995da3d4e6d25380b2b5e8091e1f106
Călina Cenan <calina@cenan.net>
2023-03-17 14:31:37 +0200

    Bump version: 1.3.12 → 1.4.0 (#619)

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ setup(
     url="https://github.com/oceanprotocol/provider-py",
     # fmt: off
     # bumpversion needs single quotes
-    version='1.3.12',
+    version='1.4.0',
     # fmt: on
     zip_safe=False,
 )
