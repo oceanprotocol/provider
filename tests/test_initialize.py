41bdeed882a7695e2f627ddc6a19dfde6a7691c6
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-04-11 11:59:21 +0300

    replace with print.

--- tests/test_initialize.py
+++ tests/test_initialize.py
@@ -37,7 +37,7 @@ logger = logging.getLogger(__name__)
 @pytest.mark.integration
 def test_initialize_on_bad_url(client, publisher_wallet, consumer_wallet, web3):
     test1_start_timestamp = datetime.now()
-    logger.info(f"test1_start_timestamp: {test1_start_timestamp}")
+    print(f"test1_start_timestamp: {test1_start_timestamp}")
     asset = get_dataset_with_invalid_url_ddo(client, publisher_wallet)
     service = get_first_service_by_type(asset, ServiceType.ACCESS)
 
@@ -51,14 +51,14 @@ def test_initialize_on_bad_url(client, publisher_wallet, consumer_wallet, web3):
     assert "error" in response.json
     assert "Asset URL not found, not available or invalid." in response.json["error"]
     test1_end_timestamp = datetime.now()
-    logger.info(f"test1_end_timestamp: {test1_end_timestamp}")
-    logger.info(f"test1 duration: {test1_end_timestamp - test1_start_timestamp}")
+    print(f"test1_end_timestamp: {test1_end_timestamp}")
+    print(f"test1 duration: {test1_end_timestamp - test1_start_timestamp}")
 
 
 @pytest.mark.integration
 def test_initialize_on_ipfs_url(client, publisher_wallet, consumer_wallet, web3):
     test2_start_timestamp = datetime.now()
-    logger.info(f"test2_start_timestamp: {test2_start_timestamp}")
+    print(f"test2_start_timestamp: {test2_start_timestamp}")
     asset = get_dataset_with_ipfs_url_ddo(client, publisher_wallet)
     service = get_first_service_by_type(asset, ServiceType.ACCESS)
     # mint_100_datatokens(
@@ -70,14 +70,14 @@ def test_initialize_on_ipfs_url(client, publisher_wallet, consumer_wallet, web3)
 
     assert datatoken == service.datatoken_address
     test2_end_timestamp = datetime.now()
-    logger.info(f"test2_end_timestamp: {test2_end_timestamp}")
-    logger.info(f"test2 duration: {test2_end_timestamp - test2_start_timestamp}")
+    print(f"test2_end_timestamp: {test2_end_timestamp}")
+    print(f"test2 duration: {test2_end_timestamp - test2_start_timestamp}")
 
 
 @pytest.mark.integration
 def test_initialize_on_disabled_asset(client, publisher_wallet, consumer_wallet, web3):
     test3_start_timestamp = datetime.now()
-    logger.info(f"test3_start_timestamp: {test3_start_timestamp}")
+    print(f"test3_start_timestamp: {test3_start_timestamp}")
     asset, real_asset = get_dataset_ddo_disabled(client, publisher_wallet)
     assert real_asset
     service = get_first_service_by_type(asset, ServiceType.ACCESS)
@@ -92,14 +92,14 @@ def test_initialize_on_disabled_asset(client, publisher_wallet, consumer_wallet,
     assert "error" in response.json
     assert response.json["error"] == "Asset malformed or disabled."
     test3_end_timestamp = datetime.now()
-    logger.info(f"test3_end_timestamp: {test3_end_timestamp}")
-    logger.info(f"test3 duration: {test3_end_timestamp - test3_start_timestamp}")
+    print(f"test3_end_timestamp: {test3_end_timestamp}")
+    print(f"test3 duration: {test3_end_timestamp - test3_start_timestamp}")
 
 
 @pytest.mark.integration
 def test_initialize_on_unlisted_asset(client, publisher_wallet, consumer_wallet, web3):
     test4_start_timestamp = datetime.now()
-    logger.info(f"test4_start_timestamp: {test4_start_timestamp}")
+    print(f"test4_start_timestamp: {test4_start_timestamp}")
     asset, real_asset = get_dataset_ddo_unlisted(client, publisher_wallet)
     assert real_asset
     service = get_first_service_by_type(asset, ServiceType.ACCESS)
@@ -114,8 +114,8 @@ def test_initialize_on_unlisted_asset(client, publisher_wallet, consumer_wallet,
 
     assert datatoken == service.datatoken_address
     test4_end_timestamp = datetime.now()
-    logger.info(f"test4_end_timestamp: {test4_end_timestamp}")
-    logger.info(f"test4 duration: {test4_end_timestamp - test4_start_timestamp}")
+    print(f"test4_end_timestamp: {test4_end_timestamp}")
+    print(f"test4 duration: {test4_end_timestamp - test4_start_timestamp}")
 
 
 @pytest.mark.integration
@@ -123,7 +123,7 @@ def test_initialize_on_asset_with_custom_credentials(
     client, publisher_wallet, consumer_wallet, web3
 ):
     test5_start_timestamp = datetime.now()
-    logger.info(f"test5_start_timestamp: {test5_start_timestamp}")
+    print(f"test5_start_timestamp: {test5_start_timestamp}")
     asset = get_dataset_ddo_with_denied_consumer(
         client, publisher_wallet, consumer_wallet.address
     )
@@ -143,14 +143,14 @@ def test_initialize_on_asset_with_custom_credentials(
         == f"Error: Access to asset {asset.did} was denied with code: ConsumableCodes.CREDENTIAL_IN_DENY_LIST."
     )
     test5_end_timestamp = datetime.now()
-    logger.info(f"test5_end_timestamp: {test5_end_timestamp}")
-    logger.info(f"test5 duration: {test5_end_timestamp - test5_start_timestamp}")
+    print(f"test5_end_timestamp: {test5_end_timestamp}")
+    print(f"test5 duration: {test5_end_timestamp - test5_start_timestamp}")
 
 
 @pytest.mark.integration
 def test_initialize_reuse(client, publisher_wallet, consumer_wallet, web3):
     test6_start_timestamp = datetime.now()
-    logger.info(f"test6_start_timestamp: {test6_start_timestamp}")
+    print(f"test6_start_timestamp: {test6_start_timestamp}")
     asset = get_dataset_ddo_with_multiple_files(client, publisher_wallet)
 
     service = get_first_service_by_type(asset, ServiceType.ACCESS)
@@ -195,8 +195,8 @@ def test_initialize_reuse(client, publisher_wallet, consumer_wallet, web3):
     assert response.json["datatoken"] == service.datatoken_address
     assert "validOrder" not in response.json
     test6_end_timestamp = datetime.now()
-    logger.info(f"test6_end_timestamp: {test6_end_timestamp}")
-    logger.info(f"test6 duration: {test6_end_timestamp - test6_start_timestamp}")
+    print(f"test6_end_timestamp: {test6_end_timestamp}")
+    print(f"test6 duration: {test6_end_timestamp - test6_start_timestamp}")
 
 
 @pytest.mark.integration
@@ -204,7 +204,7 @@ def test_can_not_initialize_compute_service_with_simple_initialize(
     client, publisher_wallet, consumer_wallet, web3
 ):
     test7_start_timestamp = datetime.now()
-    logger.info(f"test7_start_timestamp: {test7_start_timestamp}")
+    print(f"test7_start_timestamp: {test7_start_timestamp}")
     asset_w_compute_service = get_registered_asset(
         publisher_wallet, custom_services="vanilla_compute", custom_services_args=[]
     )
@@ -222,8 +222,8 @@ def test_can_not_initialize_compute_service_with_simple_initialize(
         == "Use the initializeCompute endpoint to initialize compute jobs."
     )
     test7_end_timestamp = datetime.now()
-    logger.info(f"test7_end_timestamp: {test7_end_timestamp}")
-    logger.info(f"test7 duration: {test7_end_timestamp - test7_start_timestamp}")
+    print(f"test7_end_timestamp: {test7_end_timestamp}")
+    print(f"test7 duration: {test7_end_timestamp - test7_start_timestamp}")
 
 
 @pytest.mark.integration
@@ -235,7 +235,7 @@ def test_initialize_compute_works(
     `validOrder` for both dataset and algorithm.
     """
     test8_start_timestamp = datetime.now()
-    logger.info(f"test8_start_timestamp: {test8_start_timestamp}")
+    print(f"test8_start_timestamp: {test8_start_timestamp}")
     ddo, alg_ddo = build_and_send_ddo_with_compute_service(
         client,
         publisher_wallet,
@@ -280,8 +280,8 @@ def test_initialize_compute_works(
     assert "providerFee" in response.json["algorithm"]
     assert "validOrder" not in response.json["algorithm"]
     test8_end_timestamp = datetime.now()
-    logger.info(f"test8_end_timestamp: {test8_end_timestamp}")
-    logger.info(f"test8 duration: {test8_end_timestamp - test8_start_timestamp}")
+    print(f"test8_end_timestamp: {test8_end_timestamp}")
+    print(f"test8 duration: {test8_end_timestamp - test8_start_timestamp}")
 
 
 @pytest.mark.integration
@@ -308,7 +308,7 @@ def test_initialize_compute_order_reused(
         wrong tx id for dataset order
     """
     test9_start_timestamp = datetime.now()
-    logger.info(f"test9_start_timestamp: {test9_start_timestamp}")
+    print(f"test9_start_timestamp: {test9_start_timestamp}")
     # Order asset, valid for 30 seconds
     valid_until = get_future_valid_until(short=True)
     ddo, tx_id, alg_ddo, alg_tx_id = build_and_send_ddo_with_compute_service(
@@ -413,8 +413,8 @@ def test_initialize_compute_order_reused(
     assert "datatoken" in response.json["datasets"][0].keys()
     assert "providerFee" in response.json["datasets"][0].keys()
     test9_end_timestamp = datetime.now()
-    logger.info(f"test9_end_timestamp: {test9_end_timestamp}")
-    logger.info(f"test9 duration: {test9_end_timestamp - test9_start_timestamp}")
+    print(f"test9_end_timestamp: {test9_end_timestamp}")
+    print(f"test9 duration: {test9_end_timestamp - test9_start_timestamp}")
 
 
 @pytest.mark.integration
@@ -422,7 +422,7 @@ def test_initialize_compute_paid_env(
     client, publisher_wallet, consumer_wallet, paid_c2d_env
 ):
     test10_start_timestamp = datetime.now()
-    logger.info(f"test10_start_timestamp: {test10_start_timestamp}")
+    print(f"test10_start_timestamp: {test10_start_timestamp}")
     ddo, alg_ddo = build_and_send_ddo_with_compute_service(
         client,
         publisher_wallet,
@@ -463,26 +463,26 @@ def test_initialize_compute_paid_env(
         response.json["datasets"][0]["providerFee"]["providerFeeAmount"]
     ) >= to_wei(7)
     test10_end_timestamp = datetime.now()
-    logger.info(f"test10_end_timestamp: {test10_end_timestamp}")
-    logger.info(f"test10 duration: {test10_end_timestamp - test10_start_timestamp}")
+    print(f"test10_end_timestamp: {test10_end_timestamp}")
+    print(f"test10 duration: {test10_end_timestamp - test10_start_timestamp}")
 
 
 def test_socket():
     test11_start_timestamp = datetime.now()
-    logger.info(f"test11_start_timestamp: {test11_start_timestamp}")
+    print(f"test11_start_timestamp: {test11_start_timestamp}")
     import socket
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     s.connect(("172.15.0.13", 31000))
-    logger.info(f"socket connected successfully to op serv")
+    print(f"socket connected successfully to op serv")
     s.send(b"GET / HTTP/1.1\r\nHost:172.15.0.13\r\n\r\n")
-    logger.info(f"socket send GET request successfully to op serv")
+    print(f"socket send GET request successfully to op serv")
     response = s.recv(4096)
     s.close()
-    logger.info(f"socket closed successfully")
+    print(f"socket closed successfully")
     assert response
-    logger.info(f"response from socket op serv: {response.decode()}")
+    print(f"response from socket op serv: {response.decode()}")
     test11_end_timestamp = datetime.now()
-    logger.info(f"test10_end_timestamp: {test11_end_timestamp}")
-    logger.info(f"test10 duration: {test11_end_timestamp - test11_start_timestamp}")
+    print(f"test10_end_timestamp: {test11_end_timestamp}")
+    print(f"test10 duration: {test11_end_timestamp - test11_start_timestamp}")
