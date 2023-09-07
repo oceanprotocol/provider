651eb0ccdd03a594382bbcfd934fbd59ad16f433
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-09-07 10:12:31 +0300

    Modified tests. Bump version to v2.1.3.

--- tests/helpers/nonce.py
+++ tests/helpers/nonce.py
@@ -1,18 +1,19 @@
 import time
+from _decimal import Decimal
 
 from ocean_provider.user_nonce import get_nonce, update_nonce
 
 
-def build_nonce(address) -> int:
+def build_nonce(address) -> Decimal:
     nonce = get_nonce(address)
     if nonce:
-        nonce = int(float(nonce)) + 1
+        nonce = Decimal(nonce) + 1
         update_nonce(address, nonce)
 
-        return int(nonce)
+        return Decimal(nonce)
 
     update_nonce(address, 1)
-    return 1
+    return Decimal(1)
 
 
 def build_nonce_for_compute() -> int:
